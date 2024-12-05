1c5cb208ae088b880430c7c94a22489ddb8cde22
ayfokir <ayfotsega19.27@gmail.com>
2024-12-05 16:27:49 +0300

    remove unused hooks ,useContext

--- components/notification/Notification.tsx
+++ components/notification/Notification.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-import React, { useContext, useEffect } from 'react';
+import React, {  useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // import NotificationContext from "@/Context/notificationContext";

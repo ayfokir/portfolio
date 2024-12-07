ca73872dae078c37c829e4dc4206902296eae5a8
ayfokir <ayfotsega19.27@gmail.com>
2024-12-07 11:50:48 +0300

    correct pizza aapp url

--- components/portfolio/MyRecentWorks.tsx
+++ components/portfolio/MyRecentWorks.tsx
@@ -20,7 +20,7 @@ const MyRecentWorks = ({ id }: PropsTypes) => {
       title: "Pizza Ordering App",
       description:
         "A user-friendly pizza ordering application that allows users to customize their orders and track deliveries in real-time.",
-      link: "https://pizza-ordering-service-app-three.vercel.app/",
+      link: "https://pizza-ordering-service-app-csbo.vercel.app/",
       image: "/images/pizza.png", // Replace with the actual image path
     },
     {
